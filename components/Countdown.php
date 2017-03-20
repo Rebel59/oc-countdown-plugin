42f46a3939b20a73b7a89bea64c6c5be25580183
Antonie Hogewoning <anton@rebel59.nl>
2017-03-20 21:05:16 +0100

    Added more properties for better control

--- components/Countdown.php
+++ components/Countdown.php
@@ -23,11 +23,36 @@ class Countdown extends ComponentBase
                 'placeholder'       => 'YYYY/MM/DD HH:MM:SS',
                 'validationPattern' => '^\d\d\d\d\/(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$',
                 'validationMessage' => 'rebel59.countdown::lang.components.countdown.properties.date.validationMessage'
+            ],
+            'jquery' => [
+                'title'             => 'rebel59.countdown::lang.components.countdown.properties.jquery.title',
+                'description'       => 'rebel59.countdown::lang.components.countdown.properties.jquery.description',
+                'type'              => 'checkbox',
+                'default'           => false
+            ],
+            'countdown' => [
+                'title'             => 'rebel59.countdown::lang.components.countdown.properties.countdown.title',
+                'description'       => 'rebel59.countdown::lang.components.countdown.properties.countdown.description',
+                'type'              => 'checkbox',
+                'default'           => true
+            ],
+            'init' => [
+                'title'             => 'rebel59.countdown::lang.components.countdown.properties.init.title',
+                'description'       => 'rebel59.countdown::lang.components.countdown.properties.init.description',
+                'type'              => 'checkbox',
+                'default'           => true
+            ],
+            'css' => [
+                'title'             => 'rebel59.countdown::lang.components.countdown.properties.css.title',
+                'description'       => 'rebel59.countdown::lang.components.countdown.properties.css.description',
+                'type'              => 'checkbox',
+                'default'           => true
             ]
+
         ];
     }
 
-    public function onRun(){
+    public function onRender(){
         $this->loadAssets();
     }
 
@@ -36,10 +61,18 @@ class Countdown extends ComponentBase
             $this->page['error'] = true;
             return false;
         }
-        $this->addCss('/plugins/rebel59/countdown/assets/dist/app.countdown.min.css');
 
-        $this->addJs('/plugins/rebel59/countdown/assets/dist/vendor/countdown/jquery.countdown.min.js');
-        $this->addJs('/plugins/rebel59/countdown/assets/dist/app.countdown.min.js');
+        if($this->property('css'))
+            $this->addCss('/plugins/rebel59/countdown/assets/dist/app.countdown.min.css');
+
+        if($this->property('jquery'))
+            $this->addJs('/plugins/rebel59/countdown/assets/dist/vendor/jquery/jquery.3.2.0.production.js');
+
+        if($this->property('countdown'))
+            $this->addJs('/plugins/rebel59/countdown/assets/dist/vendor/countdown/jquery.countdown.min.js');
+
+        if($this->property('init'))
+            $this->addJs('/plugins/rebel59/countdown/assets/dist/app.countdown.min.js');
     }
 
     public function onCountdownDate(){
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -14,7 +14,24 @@ return [
                     'title' => 'Date',
                     'description' => 'The date the countdown uses.',
                     'validationMessage' => 'Invalid datetime format. Format should be YYYY/MM/DD HH:MM:SS'
+                ],
+                'jquery' => [
+                    'title' => 'Load jQuery',
+                    'description' => 'Load the jQuery library'
+                ],
+                'countdown' => [
+                    'title' => 'Load jQuery.Countdown',
+                    'description' => 'Load the jQuery.Countdown library'
+                ],
+                'init' => [
+                    'title' => 'Load standard init file',
+                    'description' => 'Load the standard initialisation file that starts the countdown.'
+                ],
+                'css' => [
+                    'title' => 'Load standard css file',
+                    'description' => 'Load the standard css file that starts styles countdown.'
                 ]
+
             ]
         ]
     ]
